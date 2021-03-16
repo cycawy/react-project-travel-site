7296b0c20c55a336a5d52a3673a5e1949d416342
cycawy <cycawy@gmail.com>
2021-03-16 12:32:21 -0700

    updated images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import ImageEight from "../images/img-8.jpg";
+import ImageOne from "../images/img-1.jpg";
+import ImageTwo from "../images/img-2.jpg";
+import ImageThree from "../images/img-3.jpg";
+import ImageFour from "../images/img-4.jpg";
+import ImageFive from "../images/img-5.jpg";
+
 
 function Cards(){
     return(
@@ -10,13 +17,15 @@ function Cards(){
                 <div className='cards__wrapper'>
                     <div className='cards__items'>
                         <CardItem 
-                            src="../images/img-8.jpg"
+                            // src="../images/img-8.jpg"
+                            src={ImageEight}
                             text='Explore this hidden waterfall in the amazon jungle'
                             label='Adventure'
                             path='/services'
                         />
                         <CardItem 
-                            src='../images/img-7.jpg'
+                            // src='../images/img-7.jpg'
+                            src={ImageOne}
                             text='Explore this hidden waterfall in the amazon jungle'
                             label='Adventure'
                             path='/services'
@@ -25,19 +34,22 @@ function Cards(){
                     </div>
                     <div className='cards__items'>
                         <CardItem 
-                            src='../images/img-2.jpg'
+                            // src='images/img-2.jpg'
+                            src={ImageTwo}
                             text='Explore this hidden waterfall in the amazon jungle'
                             label='Adventure'
                             path='/services'
                         />
                         <CardItem 
-                            src='../images/img-2.jpg'
+                            // src='images/img-2.jpg'
+                            src={ImageThree}
                             text='Explore this hidden waterfall in the amazon jungle'
                             label='Adventure'
                             path='/services'
                         />
                         <CardItem 
-                            src='../images/img-2.jpg'
+                            // src='../images/img-2.jpg'
+                            src={ImageFour}
                             text='Explore this hidden waterfall in the amazon jungle'
                             label='Adventure'
                             path='/services'
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Button } from './Button';
 import '../App.css';
 import './HeroSection.css';
+import VideoOne from '../images/video-1.mp4'
 
 function HeroSection(){
     return(
         <div className='hero-container'>
-            <video src="/videos/video-1.mp4" autoPlay loop muted></video>
+            {/* <video src="/videos/video-1.mp4" autoPlay loop muted></video> */}
+            <video src={VideoOne} autoPlay loop muted></video>
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for? </p>
             <div className='hero-btns'>
