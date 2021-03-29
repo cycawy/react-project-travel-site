0a2891bf475729f569520302dfd04d51995e17f3
cycawy <cycawy@gmail.com>
2021-03-28 23:53:53 -0700

    update footer for other pages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
-import Footer from '../Footer';
+// import Footer from '../Footer';
 
 function Home(){
     return(
         <div>
             <HeroSection />
             <Cards />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 }
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../App.css';
+// import Footer from '../Footer';
 
 function Services(){
     return(
         <div>
             <h1 className='services'>SERVICES</h1>;
+            {/* <Footer /> */}
         </div>
     )
 }
